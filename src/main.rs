82366848c740e0a36f9bfb0a212b589b3bde6fc9
Miles Wirht <Milestheperson@posteo.net>
2025-06-16 23:40:43 -0400

    Initial files

---
+++ src/main.rs
@@ -0,0 +1,47 @@
+use leetcode_core::GQLLeetcodeRequest;
+use leetcode_core::types::language::Language;
+use leetcode_core::{EditorDataRequest, QuestionRequest, init};
+use std::error::Error;
+use std::{env, fs};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    // Read your LeetCode cookies from env vars
+    let csrf = env::var("LEETCODE_CSRF_TOKEN")?;
+    let session = env::var("LEETCODE_SESSION")?;
+
+    // Initialize the HTTP client
+    init(&csrf, &session).await?;
+
+    let mut skip = 0;
+    let limit = 100;
+
+    loop {
+        // Fetch a page of questions
+        let page = QuestionRequest::new(limit, skip).send().await?;
+        let questions = page.get_questions();
+        if questions.is_empty() {
+            break;
+        }
+
+        for q in questions {
+            let slug = q.title_slug.clone();
+            // Fetch the editor data for this problem
+            let qdata = EditorDataRequest::new(slug.clone()).send().await?;
+            let lang = Language::Cpp;
+
+            // If there is a C++ snippet, write it out
+            if let Some(code) = qdata.get_editor_data_by_language(&lang) {
+                let filename = qdata.get_filename(&lang)?;
+                fs::create_dir_all("cpp")?;
+                let path = format!("cpp/{}", filename);
+                fs::write(&path, code)?;
+                println!("Saved {}", path);
+            }
+        }
+
+        skip += limit;
+    }
+
+    Ok(())
+}
